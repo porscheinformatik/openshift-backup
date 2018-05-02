1ff59cdd014b341e1cafa929857ba61f4b8bb779
Johannes Grumboeck <johannes.grumboeck@porscheinformatik.at>
2018-05-02 17:56:01 +0200

    several fixes

--- oc-backup.sh
+++ oc-backup.sh
@@ -14,7 +14,8 @@ do
   cd $i
   oc export namespace $i >ns.yml
   oc export project   $i >project.yml
-  for j in pods replicationcontrollers deploymentconfigs buildconfigs services routes pvc quota hpa secrets configmaps daemonsets deployments endpoints imagestreams ingress scheduledjobs jobs limitranges policies policybindings roles rolebindings resourcequotas replicasets serviceaccounts templates oauthclients petsets
+  #for j in pods replicationcontrollers deploymentconfigs buildconfigs services routes pvc quota hpa secrets configmaps daemonsets deployments endpoints imagestreams ingress scheduledjobs jobs limitranges policies policybindings roles rolebindings resourcequotas replicasets serviceaccounts templates oauthclients petsets
+  for j in deploymentconfigs buildconfigs services routes pvc secrets configmaps endpoints imagestreams policies policybindings roles rolebindings serviceaccounts 
   do
     mkdir $j
     cd $j
@@ -41,29 +42,31 @@ do
   do
     [ "$BACKUP" == "" ] && continue
     echo "$POD in $PROJECT has the following BACKUP label: $BACKUP"
-    mkdir -p $DIR/../$BACKUP/$PROJECT  2>/dev/null
+    mkdir -p $DIR/../$BACKUP/$PROJECT/$DC  2>/dev/null
+    DBNAME=""
     case $BACKUP in
       mysql)
-        oc -n $PROJECT exec $POD -- /usr/bin/sh -c 'PATH=$PATH:/opt/rh/mysql55/root/usr/bin:/opt/rh/rh-mysql56/root/usr/bin/ mysqldump -h 127.0.0.1 -u $MYSQL_USER --password=$MYSQL_PASSWORD $MYSQL_DATABASE' >$DIR/../mysql/$PROJECT/$DC.sql
+        DBNAME=$(oc -n $PROJECT exec $POD -- /usr/bin/sh -c 'echo $MYSQL_DATABASE')
+        echo "Backup database $DBNAME..."
+        oc -n $PROJECT exec $POD -- /usr/bin/sh -c 'PATH=$PATH:/opt/rh/mysql55/root/usr/bin:/opt/rh/rh-mysql56/root/usr/bin/ mysqldump -h 127.0.0.1 -u $MYSQL_USER --password=$MYSQL_PASSWORD $MYSQL_DATABASE' >$DIR/../mysql/$PROJECT/$DC/$DBNAME.sql
         ;;
       postgresql)
-        oc -n $PROJECT exec $POD -- /usr/bin/sh -c 'LD_LIBRARY_PATH=/opt/rh/rh-postgresql95/root/usr/lib64 PATH=$PATH:/opt/rh/rh-postgresql95/root/usr/bin pg_dump -Fc $POSTGRESQL_DATABASE ' >$DIR/../postgresql/$PROJECT/$DC.pg_dump_custom
+        DBNAME=$(oc -n $PROJECT exec $POD -- /usr/bin/sh -c 'echo $POSTGRESQL_DATABASE')
+        echo "Backup database $DBNAME..."
+        oc -n $PROJECT exec $POD -- /usr/bin/sh -c 'LD_LIBRARY_PATH=/opt/rh/rh-postgresql95/root/usr/lib64 PATH=$PATH:/opt/rh/rh-postgresql95/root/usr/bin pg_dump -Fc $POSTGRESQL_DATABASE ' >$DIR/../postgresql/$PROJECT/$DC/$DBNAME.pg_dump_custom
         ;;
       mongodb)
-        oc -n $PROJECT exec $POD -- /usr/bin/sh -c 'PATH=$PATH:/opt/rh/rh-mongodb32/root/usr/bin mongodump -u $MONGODB_USER -p $MONGODB_PASSWORD -d $MONGODB_DATABASE --gzip --archive' >$DIR/../mongodb/$PROJECT/$DC.mongodump.gz
+        DBNAME=$(oc -n $PROJECT exec $POD -- /usr/bin/sh -c 'echo $MONGODB_DATABASE')
+        echo "Backup database $DBNAME..."
+        oc -n $PROJECT exec $POD -- /usr/bin/sh -c 'PATH=$PATH:/opt/rh/rh-mongodb32/root/usr/bin mongodump -u $MONGODB_USER -p $MONGODB_PASSWORD -d $MONGODB_DATABASE --gzip --archive' >$DIR/../mongodb/$PROJECT/$DC/$DBNAME.mongodump.gz
         ;;
       fs)
+        test -z "$BACKUPVOLUMEMOUNT" && echo "ERROR: Label 'backupvolumemount' not defined!" && continue
         FS=$(oc -n $PROJECT volume pod/$POD --name "$BACKUPVOLUMEMOUNT"|grep "mounted at"|awk '{print $NF}')
         if oc -n $PROJECT exec $POD -- test -d $FS
         then
-          mkdir -p $DIR/../fs/$PROJECT/$FS/
-          # check if full rsync is available in container else failback to "oc rsync"
-          if oc -n $PROJECT exec $POD -- rsync --version >/dev/null 2>&1
-          then
-            rsync -az --delete --rsh="oc -n $PROJECT rsh" $POD:$FS/ $DIR/../fs/$PROJECT/$FS/
-          else
-            oc -n $PROJECT rsync --delete=true --quiet $POD:$FS/ $DIR/../fs/$PROJECT/$FS/
-          fi
+          mkdir -p $DIR/../fs/$PROJECT/$DC/$FS/
+          oc -n $PROJECT rsync --delete=true --quiet $POD:$FS/ $DIR/../fs/$PROJECT/$DC/$FS/
         else
           echo "ERROR: FS $FS is no valid directory in POD $POD!"
         fi
